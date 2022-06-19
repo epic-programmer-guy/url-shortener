e9233f2fa14fd34d8c728572e4961a4c0f0dfc68
epic-programmer-guy <zanzan.yj@gmail.com>
2022-06-19 07:44:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# URL Shortener
+
+This is a simple URL shortener using the [Gin](https://github.com/gin-gonic/gin) framework and the [GORM](https://gorm.io/) ORM.
+To make the links as short as reasonably possible only 65536 can be created, meaning that this is not suitable for public websites etc.
+Additionally, the only way to add links is to use an api endpoint.
+
+To use this project you must add a configuration file as such to the folder in which the binary lies.
+
+## config.json
+```
+{
+    "prefix": "placeholder/",
+    "db": "test.db",
+    "password": "placeholder"
+}
+```
+
+You can choose to leave the prefix empty, however choosing a password, a filename for the sqlite database is required.
+
+Additionally you can add an HTML file called badrequest.html to the subfolder ```templates```, which will be displayed when an invalid link beginning with the specified prefix is opened by a user.
+Resources for this website, such as stylesheets or images may be placed in the resources subdirectory, which is statically routed.
+
+## Usage
+The API endpoint to add links is ```127.0.0.1:8080/placeholder/add```
+Add new links by POSTing a form data containing the link as "link" and the password as "password".
+A JSON containing the shortened address will be returned to you, unless an error has occured.
+I recommend [Postman](https://www.postman.com/) for ease of use.
+
+### Example
+![alt postman screenshot](https://i.imgur.com/CyBBcCo.png)
---
+++ main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type LinkRequest struct {
+	Address  string `form:"address" json:"address" xml:"address" binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required"`
+}
+
+type Link struct {
+	gorm.Model
+	Id     uint16
+	Target string
+}
+
+type Config struct {
+	Prefix   string `json:"prefix"`
+	db       *gorm.DB
+	DBname   string `json:"db"`
+	Password string `json:"password"`
+}
+
+func main() {
+	file, err := ioutil.ReadFile("config.json")
+
+	if err != nil {
+		log.Fatalln("config.json not found")
+	}
+
+	var config Config
+
+	json.Unmarshal(file, &config) // Initialize the configuration from the config.json file
+
+	println("URL shortener address prefix: /" + config.Prefix)
+
+	if config.Password == "" {
+		log.Fatalln("no password specified in config.json")
+	}
+
+	println("password: " + config.Password[0:1] + "*********")
+
+	if config.DBname == "" {
+		log.Fatalln("no database file specified in config.json")
+	}
+
+	println("using database " + config.DBname)
+
+	db, err := gorm.Open(sqlite.Open(config.DBname), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln("failed to connect database")
+	}
+	config.db = db
+	db.AutoMigrate(&Link{})
+
+	rand.Seed(time.Now().UnixNano())
+
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.Default()
+	router.SetTrustedProxies(nil) // If you're using a CDN or some other kind of proxy this does not work.
+
+	//
+	// Loads all HTML templates within the templates folder
+	//
+	router.LoadHTMLGlob("templates/*") //LoadHTMLGlob enables loading a batch of templates at once
+
+	///
+	/// API endpoint for adding the links
+	///
+	router.POST("/"+config.Prefix+"add", func(context *gin.Context) {
+		requestBody := LinkRequest{}
+		context.Bind(&requestBody)
+
+		if requestBody.Password != config.Password { // Exits if the user does not know the password
+			context.JSON(http.StatusUnauthorized, gin.H{})
+			return
+		}
+
+		link := Link{Target: "some random text"}
+		var rng uint16
+
+		for link.Target != "" {
+			rng = uint16(rand.Uint32())
+			link.Target = ""               // Set target to an empty string...
+			db.First(&link, "id = ?", rng) // ...except if the id happens to already be in use
+		}
+
+		link.Id = rng
+		link.Target = requestBody.Address
+
+		db.Create(&link)
+		db.Save(&link)
+
+		context.JSON(200, gin.H{
+			"address": "/" + config.Prefix + parseIdInt(rng),
+		})
+	})
+
+	///
+	/// Redirect
+	///
+	router.GET("/"+config.Prefix+":id", func(context *gin.Context) {
+		var link Link
+		i, err := parseIdString(context.Param("id"))
+
+		if err != nil {
+			context.HTML(http.StatusBadRequest, "badrequest.html", gin.H{"title": "400 - Bad Request"})
+			return
+		}
+
+		db.First(&link, "id = ?", i)
+
+		if link.Target == "" {
+			context.HTML(http.StatusBadRequest, "badrequest.html", gin.H{"title": "400 - Bad Request"})
+			return
+		}
+
+		context.Redirect(http.StatusMovedPermanently, link.Target)
+	})
+
+	///
+	/// /prefix/
+	///
+	router.GET("/"+config.Prefix, func(context *gin.Context) {
+		context.HTML(http.StatusBadRequest, "badrequest.html", gin.H{"title": "400 - Bad Request"})
+	})
+
+	///
+	///	static route for all resources within the "resources" folder
+	///
+	router.Static("/resources", "./resources")
+
+	router.Run(":8080")
+}
+
+func parseIdString(idString string) (uint16, error) {
+	i, err := strconv.ParseUint(idString, 16, 32)
+	return uint16(i), err
+}
+
+func parseIdInt(idInt uint16) string {
+	return strconv.FormatUint(uint64(idInt), 16)
+}
